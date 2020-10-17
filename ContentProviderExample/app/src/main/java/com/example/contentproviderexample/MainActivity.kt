8cf8149070fdc46d5ed3c1e47a1cbe5e689dea38
LeluRezt <lelurezt@gmail.com>
2020-10-17 19:39:31 +0700

    Add permission request to access SD card

--- ContentProviderExample/app/src/main/java/com/example/contentproviderexample/MainActivity.kt
+++ ContentProviderExample/app/src/main/java/com/example/contentproviderexample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.contentproviderexample
 
-import android.content.ContentProvider
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.example.contentproviderexample.databinding.ActivityMainBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,7 +26,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
-        getCursorOfVideo()
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        Dexter.withContext(this)
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(object : PermissionListener {
+                override fun onPermissionGranted(response : PermissionGrantedResponse?) {
+                    Log.i(TAG, "Permission Granted")
+                    getCursorOfVideo()
+                }
+
+                override fun onPermissionDenied(response : PermissionDeniedResponse?) {
+                    Log.i(TAG, "Permission Denied")
+                    if (response?.isPermanentlyDenied!!) {
+                        Log.i(TAG, "Permission Permanent Denied")
+                        openSettingPermission()
+                    }
+                    finish()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    respone : PermissionRequest?,
+                    token : PermissionToken?
+                ) {
+                    Log.i(TAG, "This app need permission for read memory card")
+                    token?.continuePermissionRequest()
+                }
+
+            }).check()
+    }
+
+    private fun openSettingPermission() {
+        Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).let {
+            val uri = Uri.fromParts("package", packageName, null)
+            it.data = uri
+            startActivityForResult(it, 101)
+        }
     }
 
     private fun getCursorOfVideo() {
@@ -49,5 +94,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requestPermissions()
     }
 }
